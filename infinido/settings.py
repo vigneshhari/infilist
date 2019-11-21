18099f43d294d49d9ad7c2a242e1f19215f6425a
Vignesh Hari <vichuhari100@gmail.com>
2019-11-21 23:36:36 +0530

    Turned Debug Off

--- infinido/settings.py
+++ infinido/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "ckv*k6ir&a__jeh*yo5s)#!-w$=rqklhs1q7o9&sx89bogpun3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
